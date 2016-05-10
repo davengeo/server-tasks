4841603a36fdcabc07d5181c272f2498914cfcd1
David Navarro <id843270@proximus.com>
2016-05-10 12:02:54 +0200

    with spring boot skeleton

---
+++ src/main/java/org/daven/infinispan/Application.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Proximus.
+ * me@davengeo.com
+ */
+package org.daven.infinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
--- src/main/java/org/daven/infinispan/Runner.java
+++ src/main/java/org/daven/infinispan/Runner.java
@@ -3,14 +3,17 @@ package org.daven.infinispan;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.client.hotrod.impl.ConfigurationProperties;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import static org.infinispan.client.hotrod.impl.ConfigurationProperties.DEFAULT_HOTROD_PORT;
 
-public class Runner {
+@Component
+public class Runner implements CommandLineRunner {
 
     public void run(String... strings) throws Exception {
         ConfigurationBuilder builder = new ConfigurationBuilder();
-        builder.addServer().host("127.0.0.1").port(ConfigurationProperties.DEFAULT_HOTROD_PORT);
+        builder.addServer().host("127.0.0.1").port(DEFAULT_HOTROD_PORT);
         // Connect to the server
         RemoteCacheManager cacheManager = new RemoteCacheManager(builder.build());
         // Obtain the remote cache
